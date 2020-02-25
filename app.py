f37c8c556de16d529bc165b45ec7e46d72b40e34
Joe Sudron <joesudron@outlook.com>
2020-02-25 12:43:34 +0000

    changed port to 5000

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",
-            port=int(os.environ.get('PORT', 8000)),
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
+            port=int(os.environ.get('PORT', 5000)),
             debug=True)
