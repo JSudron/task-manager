ba1d912684cf3380cb212f1d2d51ac9b3ac91b9c
Joe Sudron <joesudron@outlook.com>
2020-02-26 13:00:09 +0000

    added editask.html

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://joe-sudron:2PACjack83@myfirstcluster-eqlcx.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
@@ -14,13 +14,14 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html', categories=mongo.db.categories.find
-    ())
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
 
 
 @app.route('/insert_task', methods=['POST'])
@@ -28,9 +29,17 @@ def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
-    
+
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get("IP", "0.0.0.0"),
-            port=int(os.environ.get('PORT', 5000)),
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
